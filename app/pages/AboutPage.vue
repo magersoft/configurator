46646ba85e9fc08e7bfc2b6f13c85fd48aa41ea8
magersoft <magervlad@yandex.ru>
2019-04-01 16:06:01 +0300

    okey

--- app/pages/AboutPage.vue
+++ app/pages/AboutPage.vue
@@ -2,9 +2,14 @@
     <div class="container">
         <h1>About page</h1>
         <p>
-            This is the About page.
+            This is the About page. <a :href="url">Google</a>
         </p>
+        <p v-on:mouseover="onHover">Counter: {{ counter }}</p>
+        <button @click="riseCounter(5)">+ 5</button>
+        <button @click="riseCounter(10)">+ 10</button>
 
+        <input type="text" v-model="inputValue">
+        <h2>{{ inputValue }}</h2>
     </div>
 </template>
 
@@ -12,7 +17,17 @@
     export default {
         data() {
             return {
-
+                url: 'http://google.com',
+                counter: 0,
+                inputValue: 'v model'
+            }
+        },
+        methods: {
+            onHover(event) {
+                event.target.style.color = 'red';
+            },
+            riseCounter(count) {
+                this.counter += count;
             }
         }
     }
